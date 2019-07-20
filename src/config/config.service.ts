f27dec91049238bfee6dfa065bf5bb1ee837cc0c
ahmed <ahmed.saab@smaato.com>
2019-07-20 15:00:12 +0200

    feat: add config service

---
+++ src/config/config.service.ts
@@ -0,0 +1,50 @@
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = ConfigService.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated
+   * JavaScript object including the applied default values.
+   */
+  private static validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      PORT: Joi.number().default(3000),
+      WEATHER_API_ROOT: Joi.string().required(),
+      WEATHER_API_KEY: Joi.string().required(),
+      DEBUG: Joi.boolean().default(false),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
+  }
+
+  get weatherApiRoot(): string {
+    return String(this.envConfig.WEATHER_API_ROOT);
+  }
+
+  get weatherApiKey(): string {
+    return String(this.envConfig.WEATHER_API_KEY);
+  }
+
+  get debug(): boolean {
+    return Boolean(this.envConfig.DEBUG);
+  }
+}
