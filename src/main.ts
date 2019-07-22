859b5448162d03e80230788cb6d1890877570243
ahmed <ahmed.saab@smaato.com>
2019-07-22 10:27:23 +0200

    feat: add missing validation pip

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 import { DatabaseModule } from './database/database.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
   await app.get(DatabaseModule).importData();
   await app.listen(app.get(ConfigService).serverPort);
 }
